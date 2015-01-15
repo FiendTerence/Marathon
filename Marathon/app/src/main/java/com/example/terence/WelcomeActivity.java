61552659bda9bd78eb1e1fc85aa29765ae86726b
Terence <fiend.terence@gmail.com>
2015-01-15 22:38:04 +0800

    Add Welcome and Tab's BG

---
+++ Marathon/app/src/main/java/com/example/terence/WelcomeActivity.java
@@ -0,0 +1,40 @@
+package com.example.terence;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.terence.marathon.MainActivity;
+import com.example.terence.marathon.R;
+
+/**
+ * Created by terence on 2015/1/14.
+ */
+public class WelcomeActivity extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_layout);
+        mHandler.sendEmptyMessageDelayed(GOTO_MAIN_ACTIVITY, 2000);//3秒跳轉
+    }
+    private static final int GOTO_MAIN_ACTIVITY = 0;
+    private Handler mHandler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+
+            switch (msg.what) {
+                case GOTO_MAIN_ACTIVITY:
+                    Intent intent = new Intent();
+                    intent.setClass(WelcomeActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+    };
+
+}
